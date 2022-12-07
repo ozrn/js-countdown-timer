aab422806f237d141369ca703a70e02e50d51600
ozrn <ozerenkevser92@gmail.com>
2022-12-07 16:15:05 +0300

    Store interval in its own variable to stop at 0

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+let countdown;
+
 function timer(seconds){
     const now = Date.now(); // the value is in milliseconds
 
     const then = now + (seconds * 1000);
 
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if(secondsLeft < 0){  // setInterval does not know where to stop itself so we need to store interval in its own variable!
+            clearInterval(countdown);
+            return; 
+        }
 
         console.log(secondsLeft);
 
     }, 1000);
 }
 
-//timer(15);
